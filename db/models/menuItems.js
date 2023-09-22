1b6d45fd1b426b4fe7101ba25b5c7a598793b419
Shujie Ma <shujie1st@gmail.com>
2023-09-21 23:46:40 -0400

    Search menu items

--- db/models/menuItems.js
+++ db/models/menuItems.js
@@ -73,4 +73,26 @@ const remove = async (id) => {
   return menuItem.id;
 };
 
-module.exports = { getAll, getOne, create, MenuItems, updateOne, remove };
+const search = async (query) => {
+  try {
+    const menuItems = await MenuItems.find({
+      $or: [
+        { name: { $regex: query, $options: "i" } },
+        { description: { $regex: query, $options: "i" } }
+      ]
+    });
+    return menuItems;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  MenuItems,
+  updateOne,
+  remove,
+  search
+};
